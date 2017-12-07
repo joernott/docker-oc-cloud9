8313b378b0cf3e21bdabc451e13537cb984ac73d
Joern Ott <joern.ott@ott-consult.de>
2017-12-07 13:27:14 +0000

    re-add cleanup

--- src/tmp/install/oc-cloud9.sh
+++ src/tmp/install/oc-cloud9.sh
@@ -18,10 +18,5 @@ chmod a+x /tmp/install/cloud9_ssh_install.sh
 cd ${APP_HOME}
 gosu ${APP_USER}:${APP_GROUP} /tmp/install/cloud9_ssh_install.sh
 
-#cleanup git make gcc cpp fipscheck fipscheck-lib glibc-devel glibc-headers groff-base kernel-headers less \
-#        libedit libgnome-keyring libgomp libmpc mpfr openssh openssh-clients perl-Carp perl-Encode perl-Error \
-#        perl-Exporter perl-File-Path perl-File-Temp perl-Filter perl-Getopt-Long perl-Git perl-HTTP-Tiny perl-PathTools \
-#        perl-Pod-Escapes perl-Pod-Perldoc perl-Pod-Simple perl-Pod-Usage perl-Scalar-List-Utils perl-Socket \
-#        perl-Storable perl-TermReadKey perl-Text-ParseWords perl-Time-HiRes perl-Time-Local perl-constant perl-libs \
-#        perl-macros perl-parent perl-podlators perl-threads perl-threads-shared rsync perl
-        
+cleanup
+
