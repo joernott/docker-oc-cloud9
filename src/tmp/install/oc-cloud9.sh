4e5009cc1c3202b06b897b674a2f81b7eb3b8afd
Joern Ott <joern.ott@ott-consult.de>
2017-12-20 17:57:05 +0000

    Fix error messages

--- src/tmp/install/oc-cloud9.sh
+++ src/tmp/install/oc-cloud9.sh
@@ -12,10 +12,10 @@ git clone https://github.com/c9/core.git /cloud9
 cd /cloud9
 ./scripts/install-sdk.sh
 sed -e 's/127\.0\.0\.1/0.0.0.0/' -i /cloud9/configs/standalone.js
-
 curl -sSo /tmp/install/cloud9_ssh_install.sh https://raw.githubusercontent.com/c9/install/master/install.sh
 chmod a+x /tmp/install/cloud9_ssh_install.sh
 cd ${APP_HOME}
+mkdir -p /cloud9/build/standalone
 gosu ${APP_USER}:${APP_GROUP} /tmp/install/cloud9_ssh_install.sh
 
 cleanup
